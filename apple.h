5009d5c56627263638707774d4d099cc2f017bff
Niko <andrew.nikolayenko@gmail.com>
2020-03-10 17:32:07 +0200

    add apple.h

---
+++ apple.h
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <conio.h>
+#include "field.h"
+
+using namespace std;
+
+int Apple = 3;
+
+class Food {
+public:
+	void generateApple() {
+		int x = 0;
+		int y = 0;
+		do {
+
+			x = rand() % (width - 2) + 1;
+			y = rand() % (height - 2) + 1;
+
+
+		} while (Field[x + y * width] != 0);
+
+
+		Field[x + y * width] = -2;
+	}
+};
