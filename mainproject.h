13247fac9d44ff58b9dc895f95873e3d41c42330
pandrew1nik <58180112+pandrew1nik@users.noreply.github.com>
2020-03-11 17:53:13 +0200

    Update mainproject.h

--- mainproject.h
+++ mainproject.h
@@ -41,45 +41,6 @@ public:
 		cin.ignore();
 	}
 
-	void changeDirection(char key) { //зміна напряму руху персонажа(змійки)
-
-		switch (key) {
-		case 'w':
-			if (direction != 2) direction = 0;
-			break;
-		case 'd':
-			if (direction != 3) direction = 1;
-			break;
-		case 's':
-			if (direction != 4) direction = 2;
-			break;
-		case 'a':
-			if (direction != 5) direction = 3;
-			break;
-		case 'x':
-			running = false;
-			break;
-		}
-	}
-
-	void move(int dx, int dy) { //покоординатний рух персонажа(змійки)
-
-		int newx = mainx + dx;
-		int newy = mainy + dy;
-
-		if (Field[newx + newy * width] == -2) {
-			Apple++;
-			generateApple();
-		}
-		else if (Field[newx + newy * width] != 0) {
-			running = false;
-		}
-
-		mainx = newx;
-		mainy = newy;
-		Field[mainx + mainy * width] = Apple + 1;
-	}
-
 	void clearScreen() {
 		system("cls");
 	}
@@ -93,23 +54,6 @@ public:
 		} while (Field[x + y * width] != 0);
 
 		Field[x + y * width] = -2;
-	}
-
-	void update() { //оновлення ігрового поля при русі персонажа(змійки)
-		switch (direction) {
-		case 0: move(-1, 0);
-			break;
-		case 1: move(0, 1);
-			break;
-		case 2: move(1, 0);
-			break;
-		case 3: move(0, -1);
-			break;
-		}
-
-		for (int i = 0; i < width * height; i++) {
-			if (Field[i] > 0) Field[i]--;
-		}
 	}
 
 	void initField() //ініціалізація ігрового поля
